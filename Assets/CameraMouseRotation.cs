38f46715aad7aaa2b65964a7f7480de13f471a46
Gibson's macbook2019 <>
2020-04-11 11:33:16 +0800

    fix camera rotation

--- Assets/CameraMouseRotation.cs
+++ Assets/CameraMouseRotation.cs
@@ -5,12 +5,24 @@ using UnityEngine;
 public class CameraMouseRotation : MonoBehaviour
 {
     public float sensitivity = 10f;
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+
+    float rotationX;
+    float rotationY;
     // Update is called once per frame
     void Update()
     {
         var c = Camera.main.transform;
-        c.Rotate(0, Input.GetAxis("Mouse X") * sensitivity, 0);
-        c.Rotate(-Input.GetAxis("Mouse Y") * sensitivity, 0, 0);
+        //c.Rotate(0, Input.GetAxis("Mouse X") * sensitivity, 0);
+        //c.Rotate(-Input.GetAxis("Mouse Y") * sensitivity, 0, 0);
+
+
+        rotationX +=  Input.GetAxis("Mouse X") * sensitivity;
+        rotationY +=  Input.GetAxis("Mouse Y") * sensitivity;
+        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+
+        c.eulerAngles = new Vector3( -rotationY, rotationX , 0);
 
         if (Input.GetMouseButtonDown(0))
             Cursor.lockState = CursorLockMode.Locked;
